5cc4be853b15b8dec9426227e028a44c1232f7d4
prachi288 <jprachi28803@gmail.com>
2024-06-18 19:30:34 +0530

    Complete all Repository Files

---
+++ src/repository/MovieTicket-Repository.js
@@ -0,0 +1,9 @@
+const {CrudRepository} = require('./index');
+const { MovieTicket } = require('../models/index');
+class MovieTicketRepository extends CrudRepository{
+    constructor(){
+        super(MovieTicket);
+    }
+}
+
+module.exports = MovieTicketRepository;
---
+++ src/repository/index.js
@@ -0,0 +1,10 @@
+module.exports={
+    CrudRepository: require('./Crud-Repository'),
+    UserRepository: require('./User-Repository'),
+    AadharCardRepository: require('./AadharCard-Repository'),
+    PANCardRepository: require('./PanCard-Repository'),
+    MovieTicketRepository: require('./MovieTicket-Repository'),
+    TrainTicketRepository: require('./TrainTicket-Repository'),
+    XIIMarkSheetRepository: require('./XIIMarksheet-Repository'),
+    XMarkSheetRepository: require('./XMarksheet-Repository'),
+}
