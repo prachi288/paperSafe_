cd608034461ec2607e92f8bbcbc5fbef2970a3d3
prachi288 <jprachi28803@gmail.com>
2024-06-23 23:48:41 +0530

    Updated AadharCard Repository

--- src/repository/AadharCard-Repository.js
+++ src/repository/AadharCard-Repository.js
@@ -5,6 +5,29 @@ class AadhaarCardRepository extends CrudRepository{
     constructor(){
         super(AadhaarCard);
     }
+
+    async remove(userID){
+        try {
+            await this.model.deleteOne({
+                userID: userID
+            });
+            return true;
+        } catch (error) {
+            console.log("Error in Aadhar Repository Layer");
+            throw {error};
+        }
+    }
+
+    async getByUserId(userID){
+        try {
+            const response = await this.model.findOne({userID});
+            return response;
+        } catch (error) {
+            console.log("Error in Aadhar Repository Layer");
+            throw {error};
+        }
+    }
+
 }
 
 module.exports = AadhaarCardRepository;
