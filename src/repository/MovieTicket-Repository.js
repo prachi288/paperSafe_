f6ce5c6ef31e45cdbe09ccefe7e98f83887597eb
prachi288 <jprachi28803@gmail.com>
2024-06-25 23:11:33 +0530

    [Fix] MovieTicket model and MovieTicket Repository

--- src/repository/MovieTicket-Repository.js
+++ src/repository/MovieTicket-Repository.js
@@ -4,6 +4,28 @@ class MovieTicketRepository extends CrudRepository{
     constructor(){
         super(MovieTicket);
     }
+    
+    async remove(userID){
+        try {
+            await this.model.deleteOne({
+                userID: userID
+            });
+            return true;
+        } catch (error) {
+            console.log("Error in Movie Ticket Repository Layer");
+            throw {error};
+        }
+    }
+
+    async getByUserID(userID){
+        try {
+            const response = await this.model.find({userID});
+            return response;
+        } catch (error) {
+            console.log("Error in Movie Ticket Repository Layer");
+            throw {error};
+        }
+    }
 }
 
 module.exports = MovieTicketRepository;
