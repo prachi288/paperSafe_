a83085ffda64d2658d758f8b22e770bdb4fa5388
prachi288 <jprachi28803@gmail.com>
2024-06-26 00:23:25 +0530

    [Fix] XIIMarksheet Schema & XIIMarksheet Repository

--- src/repository/XIIMarksheet-Repository.js
+++ src/repository/XIIMarksheet-Repository.js
@@ -5,6 +5,28 @@ class XIIMarkSheetRepository extends CrudRepository{
     constructor(){
         super(XIIMarkSheet);
     }
+
+    async remove(userID){
+        try {
+            await this.model.deleteOne({
+                userID: userID
+            });
+            return true;
+        } catch (error) {
+            console.log("Error in XIImarksheet Repository Layer");
+            throw {error};
+        }
+    }
+
+    async getByUserId(userID){
+        try {
+            const response = await this.model.findOne({userID});
+            return response;
+        } catch (error) {
+            console.log("Error in XIImarksheet Repository Layer");
+            throw {error};
+        }
+    }
 }
 
 module.exports = XIIMarkSheetRepository;
