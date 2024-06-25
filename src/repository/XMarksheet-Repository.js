7703896d66dfda24485938dc2403becf45281a44
prachi288 <jprachi28803@gmail.com>
2024-06-26 00:02:28 +0530

    [Fix] X-Marksheet schema & X-Marksheet Repository

--- src/repository/XMarksheet-Repository.js
+++ src/repository/XMarksheet-Repository.js
@@ -5,6 +5,28 @@ class XMarkSheetRepository extends CrudRepository{
     constructor(){
         super(XMarkSheet);
     }
+
+    async remove(userID){
+        try {
+            await this.model.deleteOne({
+                userID: userID
+            });
+            return true;
+        } catch (error) {
+            console.log("Error in Xmarksheet Repository Layer");
+            throw {error};
+        }
+    }
+
+    async getByUserId(userID){
+        try {
+            const response = await this.model.findOne({userID});
+            return response;
+        } catch (error) {
+            console.log("Error in Xmarksheet Repository Layer");
+            throw {error};
+        }
+    }
 }
 
 module.exports = XMarkSheetRepository;
